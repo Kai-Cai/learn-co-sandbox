2cb74085ca68c82f4192da19cc37219f8cafdbb5
Kai-Cai <kaiaschiff@gmail.com>
2018-06-15 17:21:08 +0000

    automatically backed up by learn

---
+++ application_controller.rb
@@ -0,0 +1,16 @@
+require 'bundler'
+Bundler.require
+class MyApp < Sinatra::Base
+
+  get '/' do
+    erb :tasks
+  end
+  post '/tasks' do
+
+  @user_task= params[:name]
+  @user_time= params[:TimeDue]
+  @user_class= params[:Class]
+
+  erb :tasks
+  end
+end
